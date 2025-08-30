78084b28ccbf4c585dcc56a24f09ce0b492b2201
skinksteek <linusigelstrom@gmail.com>
2025-08-30 22:31:30 +0200

    route for matkasse project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/matkasse",
+        destination: "https://matkasse-theta.vercel.app/",
+      },
+      {
+        source: "/matkasse/:path*",
+        destination: "https://matkasse-theta.vercel.app/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
